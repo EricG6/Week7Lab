196cf259fa9e2d95fe105c136087bf25f39dfaf6
User <User@192.168.2.70>
2022-10-30 19:02:03 -0600

    Finalized

---
+++ src/java/services/RoleService.java
@@ -0,0 +1,27 @@
+package services;
+
+import dataaccess.RoleDB;
+import java.util.List;
+import models.Role;
+
+/**
+ *
+ * @author Eric
+ */
+public class RoleService {
+
+    public List<Role> getAllRoles() throws Exception {
+        RoleDB roleDB = new RoleDB();
+        List<Role> roles = roleDB.getAllRoles();
+        return roles;
+    }
+
+    public int getRoleID(Role role) {
+        String name = role.getRoleName();
+        if (name.equals("system admin")) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
---
+++ src/java/services/UserService.java
@@ -0,0 +1,54 @@
+package services;
+
+import dataaccess.UserDB;
+import java.util.List;
+import models.Role;
+import models.User;
+
+/**
+ *
+ * @author Eric
+ */
+public class UserService {
+
+    public List<User> getAllUsers() throws Exception {
+        UserDB userDB = new UserDB();
+        List<User> users = userDB.getAllUsers();
+        return users;
+    }
+
+    public User getUser(String email) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = userDB.getUser(email);
+        return user;
+    }
+
+    public void insertUser(String email, String fname, String lname, String pass,
+            Role role) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = new User(email, fname, lname, pass, role);
+        userDB.insertUser(user);
+    }
+
+    public void updateUser(String email, String fname, String lname, String pass,
+            Role role) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = new User(email, fname, lname, pass, role);
+        userDB.updateUser(user);
+    }
+
+    public void deleteUser(String email) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = new User();
+        user.setEmail(email);
+        userDB.deleteUser(user);
+    }
+
+    public boolean isNotEmpty(String email, String firstname, String lastname, String password) {
+        if (email == null || email.equals("") || firstname == null || firstname.equals("") || lastname == null || lastname.equals("") || password == null || password.equals("")) {
+            return false;
+        }
+        return true;
+    }
+
+}
